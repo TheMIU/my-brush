a1327d7670ee1d7013be9916f822bfc64f57b348
Kasun Miuranga <kasunmiuranga2000@gmail.com>
2024-08-22 17:15:45 +0530

    undo redo

--- script.js
+++ script.js
@@ -6,9 +6,12 @@ canvas.height = window.innerHeight * 0.8;
 
 let painting = false;
 let currentColor = 'black';
+let undoStack = [];
+let redoStack = [];
 
 function startPosition(e) {
     painting = true;
+    saveState(undoStack, redoStack);
     draw(e);
 }
 
@@ -35,8 +38,48 @@ function setColor(color) {
 }
 
 function clearCanvas() {
+    saveState(undoStack, redoStack);
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
+
+function saveState(undoStack, redoStack, keepRedo = false) {
+    if (!keepRedo) {
+        redoStack.length = 0;
+    }
+    undoStack.push(canvas.toDataURL());
+}
+
+function undo() {
+    restoreState(undoStack, redoStack);
+}
+
+function redo() {
+    restoreState(redoStack, undoStack);
+}
+
+function restoreState(popStack, pushStack) {
+    if (popStack.length) {
+        saveState(pushStack, popStack, true);
+        const restoreState = popStack.pop();
+        const img = new Image();
+        img.src = restoreState;
+        img.onload = function () {
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            ctx.drawImage(img, 0, 0);
+        };
+    }
+}
+
+// Add key bindings for Ctrl+Z (undo) and Ctrl+Shift+Z (redo)
+document.addEventListener('keydown', function (e) {
+    if (e.ctrlKey && e.key === 'z' && !e.shiftKey) {
+        e.preventDefault();
+        undo();
+    } else if (e.ctrlKey && e.key === 'Z' && e.shiftKey) {
+        e.preventDefault();
+        redo();
+    }
+});
 
 canvas.addEventListener('mousedown', startPosition);
 canvas.addEventListener('mouseup', endPosition);
